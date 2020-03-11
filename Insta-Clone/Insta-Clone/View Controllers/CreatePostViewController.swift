730f00ddf0e49da0f3b71ba7af4834ff7231edb1
Gregory Keeley <gregorykeeley@pursuit.org>
2020-03-11 12:39:30 -0400

    - Started building "CreatePostVC"

--- Insta-Clone/Insta-Clone/View Controllers/CreatePostViewController.swift
+++ Insta-Clone/Insta-Clone/View Controllers/CreatePostViewController.swift
@@ -10,13 +10,46 @@ import UIKit
 
 class CreatePostViewController: UIViewController {
 
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    private let dbService = DatabaseService()
+    private let storageService = StorageService()
+    
+    private var selectedImage: UIImage? {
+        didSet {
+            postImageView.image = selectedImage
+        }
+    }
+    private lazy var tapGesture: UIGestureRecognizer = {
+        let gesture = UITapGestureRecognizer()
+        gesture.addTarget(self, action: #selector(selectImage(_:)))
+        return gesture
+    }()
+    private lazy var imagePickerController: UIImagePickerController = {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        return picker
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-
-
+    private func setupUI() {
+        postImageView.isUserInteractionEnabled = true
+        postImageView.addGestureRecognizer(tapGesture)
+    }
+    @objc private func selectImage(_ sender: UIButton) {
+        print("tapp")
+        
+    }
+}
+extension CreatePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("could not attain image")
+        }
+        selectedImage = image
+        dismiss(animated: true)
+    }
 }
